dd836d9ac9a751c779279ce57e5444d70213567c
Shubhendu <shubhenduanupam@gmail.com>
2023-04-01 11:01:02 +0530

    Added 1000 most common english words, as text file (common_1000_words.txt) and take that as a constant List.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from tkinter import *
 
+# -------------------- CONSTANTS --------------------------#
+WIDTH = 800
+HEIGHT = 500
+FONT = ("Century", 16, "normal")
+
+with open("common_1000_words.txt", "r") as word_file:
+    words = word_file.readlines()
+
+WORDS = [word.strip() for word in words]
+
+# --------------------- FUNCTIONALITY ---------------------------#
+
+
+# ---------------------- USER INTERFACE -------------------------#
+
+print(WORDS)
